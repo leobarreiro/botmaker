0c303032e6e9466511b86b893b633830097a00cc
Leopoldo Barreiro <leopoldo.java@gmail.com>
2016-11-14 16:48:46 -0200

    Adding Gallery and Photo entities to EJB module.

--- botmaker-ejb/src/main/java/com/javaleo/systems/botmaker/ejb/entities/User.java
+++ botmaker-ejb/src/main/java/com/javaleo/systems/botmaker/ejb/entities/User.java
@@ -11,6 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
@@ -19,8 +21,15 @@ import javax.persistence.Table;
 import org.javaleo.libs.jee.core.model.IEntityBasic;
 
 @Entity
-@Table(schema = EntityUtils.SCHEMA, name = "user")
-@SequenceGenerator(schema = EntityUtils.SCHEMA, name = "sq_user", sequenceName = "user_seq", allocationSize = 1, initialValue = 1)
+@Table(
+		schema = EntityUtils.SCHEMA,
+		name = "user")
+@SequenceGenerator(
+		schema = EntityUtils.SCHEMA,
+		name = "sq_user",
+		sequenceName = "user_seq",
+		allocationSize = 1,
+		initialValue = 1)
 public class User implements IEntityBasic {
 
 	private static final long serialVersionUID = 1L;
@@ -35,11 +44,15 @@ public class User implements IEntityBasic {
 	private Boolean admin;
 	private Boolean active;
 	private List<UserPreference> preferences;
+	private List<Gallery> galleries;
 
 	@Override
 	@Id
-	@Column(name = "user_id")
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sq_user")
+	@Column(
+			name = "user_id")
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "sq_user")
 	public Long getId() {
 		return id;
 	}
@@ -48,7 +61,9 @@ public class User implements IEntityBasic {
 		this.id = id;
 	}
 
-	@Column(name = "name", length = 80)
+	@Column(
+			name = "name",
+			length = 80)
 	public String getName() {
 		return name;
 	}
@@ -57,7 +72,9 @@ public class User implements IEntityBasic {
 		this.name = name;
 	}
 
-	@Column(name = "username", length = 30)
+	@Column(
+			name = "username",
+			length = 30)
 	public String getUsername() {
 		return username;
 	}
@@ -66,7 +83,9 @@ public class User implements IEntityBasic {
 		this.username = username;
 	}
 
-	@Column(name = "passphrase", length = 128)
+	@Column(
+			name = "passphrase",
+			length = 128)
 	public String getPassword() {
 		return password;
 	}
@@ -75,7 +94,9 @@ public class User implements IEntityBasic {
 		this.password = password;
 	}
 
-	@Column(name = "email", length = 80)
+	@Column(
+			name = "email",
+			length = 80)
 	public String getEmail() {
 		return email;
 	}
@@ -84,7 +105,9 @@ public class User implements IEntityBasic {
 		this.email = email;
 	}
 
-	@Column(name = "telegram_id", nullable = true)
+	@Column(
+			name = "telegram_id",
+			nullable = true)
 	public Integer getIdTelegram() {
 		return idTelegram;
 	}
@@ -93,8 +116,14 @@ public class User implements IEntityBasic {
 		this.idTelegram = idTelegram;
 	}
 
-	@ManyToOne(fetch = FetchType.EAGER, optional = true)
-	@JoinColumn(name = "company_id", referencedColumnName = "company_id", foreignKey = @ForeignKey(name = "fk_user_company"))
+	@ManyToOne(
+			fetch = FetchType.EAGER,
+			optional = true)
+	@JoinColumn(
+			name = "company_id",
+			referencedColumnName = "company_id",
+			foreignKey = @ForeignKey(
+					name = "fk_user_company"))
 	public Company getCompany() {
 		return company;
 	}
@@ -103,7 +132,8 @@ public class User implements IEntityBasic {
 		this.company = company;
 	}
 
-	@Column(name = "admin")
+	@Column(
+			name = "admin")
 	public Boolean getAdmin() {
 		return admin;
 	}
@@ -112,7 +142,8 @@ public class User implements IEntityBasic {
 		this.admin = admin;
 	}
 
-	@Column(name = "active")
+	@Column(
+			name = "active")
 	public Boolean getActive() {
 		return active;
 	}
@@ -121,13 +152,40 @@ public class User implements IEntityBasic {
 		this.active = active;
 	}
 
-	@OneToMany(mappedBy = "user", cascade = { CascadeType.REMOVE }, fetch = FetchType.LAZY)
+	@OneToMany(
+			mappedBy = "user",
+			cascade = { CascadeType.REMOVE },
+			fetch = FetchType.LAZY)
 	public List<UserPreference> getPreferences() {
 		return preferences;
 	}
 
 	public void setPreferences(List<UserPreference> preferences) {
 		this.preferences = preferences;
+	}
+
+	@ManyToMany(
+			cascade = { CascadeType.REFRESH },
+			fetch = FetchType.EAGER)
+	@JoinTable(
+			schema = EntityUtils.SCHEMA,
+			name = "user_gallery",
+			joinColumns = { @JoinColumn(
+					name = "user_id",
+					referencedColumnName = "user_id",
+					foreignKey = @ForeignKey(
+							name = "user_fk_user")) },
+			inverseJoinColumns = { @JoinColumn(
+					name = "gallery_id",
+					referencedColumnName = "gallery_id",
+					foreignKey = @ForeignKey(
+							name = "gallery_fk_gallery")) })
+	public List<Gallery> getGalleries() {
+		return galleries;
+	}
+
+	public void setGalleries(List<Gallery> galleries) {
+		this.galleries = galleries;
 	}
 
 	@Override
@@ -147,53 +205,34 @@ public class User implements IEntityBasic {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
 		User other = (User) obj;
 		if (active == null) {
-			if (other.active != null)
-				return false;
-		} else if (!active.equals(other.active))
-			return false;
+			if (other.active != null) return false;
+		} else if (!active.equals(other.active)) return false;
 		if (admin == null) {
-			if (other.admin != null)
-				return false;
-		} else if (!admin.equals(other.admin))
-			return false;
+			if (other.admin != null) return false;
+		} else if (!admin.equals(other.admin)) return false;
 		if (company == null) {
-			if (other.company != null)
-				return false;
-		} else if (!company.equals(other.company))
-			return false;
+			if (other.company != null) return false;
+		} else if (!company.equals(other.company)) return false;
 		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
+			if (other.email != null) return false;
+		} else if (!email.equals(other.email)) return false;
 		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
+			if (other.id != null) return false;
+		} else if (!id.equals(other.id)) return false;
 		if (idTelegram == null) {
-			if (other.idTelegram != null)
-				return false;
-		} else if (!idTelegram.equals(other.idTelegram))
-			return false;
+			if (other.idTelegram != null) return false;
+		} else if (!idTelegram.equals(other.idTelegram)) return false;
 		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
+			if (other.name != null) return false;
+		} else if (!name.equals(other.name)) return false;
 		if (username == null) {
-			if (other.username != null)
-				return false;
-		} else if (!username.equals(other.username))
-			return false;
+			if (other.username != null) return false;
+		} else if (!username.equals(other.username)) return false;
 		return true;
 	}
 
---
+++ botmaker-ejb/src/main/java/com/javaleo/systems/botmaker/ejb/filters/PhotoFilter.java
@@ -0,0 +1,37 @@
+package com.javaleo.systems.botmaker.ejb.filters;
+
+import java.io.Serializable;
+
+public class PhotoFilter implements Serializable {
+
+	private static final long serialVersionUID = -6881167307992073386L;
+
+	private String name;
+	private String description;
+	private String mimeType;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getMimeType() {
+		return mimeType;
+	}
+
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+
+}
